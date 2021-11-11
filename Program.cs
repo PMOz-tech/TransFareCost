98b8f0992a67835733d603fc6f0b727b7f8c2ae9
PMOz-tech <ozon.oghenemaro@gmail.com>
2021-11-11 15:44:24 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+
+namespace SolidPrinciple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PremiumRider john = new PremiumRider();
+            Console.WriteLine($"You are a premium rider and you amount is {john.GetDiscountFare()}");
+
+            BasicRider basic = new BasicRider();
+            Console.WriteLine($"You are a basic rider and your amount is {basic.GetBaseFare()}");
+        }
+    }
+}
---
+++ Rider/Rider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidPrinciple
+{
+   public class Rider : Transport
+    {
+       
+            public int RiderId { get; set; }
+            public string RiderName { get; set; }
+
+            //public double GetBaseFare()
+            //{
+            //    return 0;
+
+            //}
+
+            //public double GetDiscountFare()
+            //{
+            //    return 0;
+            //}
+        
+    }
+}
